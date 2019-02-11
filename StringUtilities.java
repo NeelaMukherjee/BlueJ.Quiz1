ec2db38e03fca9857864272cdadbdc0d5bd7a972
Neelanjana Mukherjee <neelanjanamukherjee@zipcoders-MacBook-Pro.local>
2019-02-11 11:51:06 -0500

    completed all  string methods without the remove a char method

--- StringUtilities.java
+++ StringUtilities.java
@@ -1,3 +1,5 @@
+import java.*;
+
 
 public class StringUtilities {
     /**
@@ -5,7 +7,7 @@ public class StringUtilities {
      * @return `input`
      */
     public String returnInput(String input) {
-        return null;
+        return input;
     }
 
     /**
@@ -14,7 +16,7 @@ public class StringUtilities {
      * @return the concatenation of `baseValue` and `valueToBeAdded`
      */
     public String concatenate(String baseValue, String valueToBeAdded) {
-        return null;
+        return (baseValue+valueToBeAdded);
     }
 
     /**
@@ -22,7 +24,27 @@ public class StringUtilities {
      * @return identical string with characters in opposite order
      */
     public String reverse(String valueToBeReversed) {
-        return null;
+        //String rev ="";
+        char [] rev= valueToBeReversed.toCharArray();
+        int len = valueToBeReversed.length();
+        char temp;
+        int end =len-1;
+        int start =0;
+        
+        while (end>start){
+            
+            temp= rev[start];
+            rev[start]= rev[end];
+            rev [end]=temp;
+           end--;
+           start++;
+            
+            
+            //rev.charAt(i) = valueToBeReversed.charAt(len-(i+1));
+            
+        }
+        
+        return new String(rev);
     }
 
     /**
@@ -30,7 +52,21 @@ public class StringUtilities {
      * @return middle character of `word`
      */
     public Character getMiddleCharacter(String word) {
-        return null;
+        int a;
+        //char [] middle= word.toCharArray();
+        int len = word.length();
+        
+        if(len%2==0){
+            
+           a=len/2-1;
+        }
+            
+            else {
+            
+            a=len/2;
+        }
+        
+        return word.charAt(a);
     }
 
     /**
@@ -39,6 +75,11 @@ public class StringUtilities {
      * @return `value` with char of value `charToRemove` removed
      */
     public String removeCharacter(String value, Character charToRemove) {
+        /*String new_string ="";
+        new_string= value.replace(charToRemove,"");
+        //value.substring(0,value.indexOf(charToRemove))+
+        return new_string; */
+        
         return null;
     }
 
@@ -47,6 +88,10 @@ public class StringUtilities {
      * @return last `word` in sentence
      */
     public String getLastWord(String sentence) {
-        return null;
+        String a[] = sentence.split(" ");
+        
+       
+        
+        return a[a.length-1];
     }
 }
